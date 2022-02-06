f4e9353cebb982de76ff2b32b2f385216f89902a
Jeri <jeriacasper@gmail.com>
2022-02-06 13:33:15 -0600

    Destructuring props at the top
    next up: mini destructuring demo

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,33 +1,38 @@
-const Greeting = (props) => {
+const Greeting = ({
+	name
+	, role
+	, age
+	, albums
+}) => {
 
-    console.log(props);
+    // console.log(props);
 
     return(
       <>
         <h2>
-            Hello, {props.name}!
+            Hello, {name}!
         </h2>
         <h3>
-            {props.name}'s role in the band is {props.role}.
+            {name}'s role in the band is {role}.
         </h3>
         <h3>
-            When the band formed, {props.name} was approximately {props.age} years old.
+            When the band formed, {name} was approximately {age} years old.
         </h3>
         <h3>
-					If {props.name} is still alive, he is approximately {props.age + (2021-1967)} years old.
+					If {name} is still alive, he is approximately {age + (2021-1967)} years old.
         </h3>
-        <h3>{props.name} contributed to these albums:</h3>
+        <h3>{name} contributed to these albums:</h3>
         <ul>
             {
-							props.albums 
-							&& [...new Set(props.albums)].map(x => (
+							albums 
+							&& [...new Set(albums)].map(x => (
 							<li
 								key={x}
 							>
 								{x}
 							</li>))
             }
-            {!props.albums && <li>Check back later.</li>}
+            {!albums && <li>Check back later.</li>}
         </ul>
       </>
     );
